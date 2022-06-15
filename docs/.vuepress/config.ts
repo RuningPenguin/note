7760ad56b559ba667f7a90072527f93f9cba6cd4
wangxu <codeqie@163.com>
2022-06-15 17:31:36 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress';
 import Nav from './nav';
 
 export default defineUserConfig({
-  base: "/doc/", // 目录前缀
+  base: "/note/", // 目录前缀
   lang: 'zh-CN', // 语言
   title: '企鹅的学习笔记', // 站点标题
   description: '企鹅的学习笔记', // 站点描述
--- docs/.vuepress/customLayout.vue
+++
@@ -1,7 +0,0 @@
-<template>456</template>
-
-<script setup>
-console.log(123);
-</script>
-
-<style scoped></style>
